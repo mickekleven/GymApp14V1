eba7230473446dd95d1d9e1dda3be14ddeac22f2
Mikael Klevengård <mickekleven@outlook.com>
2022-08-21 17:29:32 +0200

    Add related GymClass members in MemberViewModel

--- GymApp14V1/Models/ViewModels/MemberViewModel.cs
+++ GymApp14V1/Models/ViewModels/MemberViewModel.cs
@@ -7,5 +7,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public int GymClassId { get; set; }
+        public IEnumerable<GymClass> GymClasses { get; set; } = new List<GymClass>();
     }
 }
