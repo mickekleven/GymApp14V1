2123018509428f0cd413be9a545477c090b3764a
Mikael Klevengård <mickekleven@outlook.com>
2022-08-23 20:28:03 +0200

    Inprogress - with Role CRUD Implementation

--- GymApp14V1.Core/ViewModels/RoleViewModel.cs
+++ GymApp14V1.Core/ViewModels/RoleViewModel.cs
@@ -5,5 +5,6 @@
     {
         public string Id { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
+        public PageHeaderViewModel PageHeader { get; set; } = new();
     }
 }
