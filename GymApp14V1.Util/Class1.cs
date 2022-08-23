450ef749beb830b57d9b84ceed1ae3657de00dda
Mikael Klevengård <mickekleven@outlook.com>
2022-08-23 16:45:55 +0200

    Add ClientArgs helper

---
+++ GymApp14V1.Util/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace GymApp14V1.Util
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ GymApp14V1.Util/Helpers/ClientArgs.cs
@@ -0,0 +1,17 @@
+﻿namespace GymApp14V1.Util.Helpers
+{
+    public class ClientArgs
+    {
+        public const string VALIDATION_SCRIPTS = "_ValidationScriptsPartial";
+
+
+        // Roles
+
+        public const string ADMIN_ROLE = "Administrator";
+        public const string MEMBER_ROLE = "Member";
+
+
+
+
+    }
+}
