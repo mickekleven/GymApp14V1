80097ecb3e5a09406ce1f6fb4247a316243c0642
Mikael Klevengård <mickekleven@outlook.com>
2022-08-23 21:49:06 +0200

    Add ModelStateValidation

---
+++ GymApp14V1/Validations/ModelStateValidation.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace GymApp14V1.Validations
+{
+    public class ModelStateValidation : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+
+            base.OnActionExecuting(context);
+        }
+
+    }
+}
