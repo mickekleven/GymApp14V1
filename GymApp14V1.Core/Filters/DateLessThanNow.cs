08c01ae1e802d1febb03abf138d2ba6b3e490d45
Mikael Klevengård <mickekleven@outlook.com>
2022-08-24 21:50:28 +0200

    Inprogress Repository

---
+++ GymApp14V1.Core/Filters/DateLessThanNow.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GymApp14V1.Core.Filters
+{
+    public class DateLessThanNow : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+
+            var message = "Selected date and time must be greated than now";
+
+            if (value is DateTime dateTimeNow)
+            {
+                if (dateTimeNow > DateTime.Now)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult(message);
+                }
+            }
+
+            return new ValidationResult("Invalid date format");
+        }
+    }
+}
