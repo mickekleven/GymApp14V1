22c0ccadc0f07f11edb6c7ca2effd0d5fa6a228e
Mikael Klevengård <mickekleven@outlook.com>
2022-08-25 21:33:39 +0200

    Add CommonInfo

---
+++ GymApp14V1.Core/ViewModels/CommonInfoViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace GymApp14V1.Core.ViewModels
+{
+    public class CommonInfoViewModel
+    {
+        public string Paragraph { get; set; } = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non nisi est sit amet facilisis magna etiam tempor orci. Sed arcu non odio euismod lacinia at. Quisque sagittis purus sit amet volutpat.";
+        public string Paragraph01 { get; set; } = "Sed felis eget velit aliquet sagittis id. Congue quisque egestas diam in arcu cursus. Aliquam etiam erat velit scelerisque. Ullamcorper sit amet risus nullam eget felis eget nunc. Lorem donec massa sapien faucibus et molestie ac. Sed sed risus pretium quam vulputate dignissim. Montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada.";
+        public string Paragraph02 { get; set; } = "Nec nam aliquam sem et tortor consequat id porta. Quisque sagittis purus sit amet volutpat consequat mauris nunc. Cum sociis natoque penatibus et magnis. Sollicitudin tempor id eu nisl nunc. Dictum varius duis at consectetur lorem donec. Natoque penatibus et magnis dis.";
+        public string Paragraph03 { get; set; } = "Tristique senectus et netus et malesuada fames ac turpis egestas. Nec feugiat in fermentum posuere urna. Quis varius quam quisque id diam vel quam elementum pulvinar. Nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est. Aenean et tortor at risus viverra adipiscing at in. Mauris augue neque gravida in fermentum et sollicitudin ac orci. Magna sit amet purus gravida quis blandit turpis cursus in. Integer malesuada nunc vel risus commodo viverra maecenas. Malesuada fames ac turpis egestas maecenas. Est lorem ipsum dolor sit amet. Leo integer malesuada nunc vel. Habitant morbi tristique senectus et netus. Sed blandit libero volutpat sed.";
+        public string Paragraph04 { get; set; } = "Tristique senectus et netus et malesuada fames ac turpis egestas. Nec feugiat in fermentum posuere urna. Quis varius quam quisque id diam vel quam elementum pulvinar. Nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est. Aenean et tortor at risus viverra adipiscing at in. Mauris augue neque gravida in fermentum et sollicitudin ac orci. Magna sit amet purus gravida quis blandit turpis cursus in. Integer malesuada nunc vel risus commodo viverra maecenas. Malesuada fames ac turpis egestas maecenas. Est lorem ipsum dolor sit amet. Leo integer malesuada nunc vel. Habitant morbi tristique senectus et netus. Sed blandit libero volutpat sed.";
+
+
+        public PageHeaderViewModel PageHeader { get; set; } = new();
+
+    }
+}
---
+++ GymApp14V1/Controllers/CommonInfoController.cs
@@ -0,0 +1,35 @@
+﻿using GymApp14V1.Core.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymApp14V1.Controllers
+{
+    public class CommonInfoController : Controller
+    {
+        public IActionResult About()
+        {
+            var model = new CommonInfoViewModel();
+
+            model.PageHeader = new PageHeaderViewModel
+            {
+                HeadLine = "About Us",
+                SubTitle = "Eleifend quam adipiscing vitae proin sagittis nisl."
+            };
+
+            return View("../CommonInfo/About", model);
+        }
+
+        public IActionResult Privacy()
+        {
+            var model = new CommonInfoViewModel();
+
+            model.PageHeader = new PageHeaderViewModel
+            {
+                HeadLine = "Privacy",
+                SubTitle = "Eleifend quam adipiscing vitae proin sagittis nisl."
+            };
+
+            return View();
+        }
+
+    }
+}
